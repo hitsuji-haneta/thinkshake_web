e015edaf2d9230ffb68c91996557845fad2156ad
hitsuji-haneta <hitsuji.haneta1986@gmail.com>
2018-07-07 00:02:50 +0900

    Set up configurations for contentful

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,19 @@
-module.exports = {
+const {getConfigForKeys} = require('./lib/config.js')
+const ctfConfig = getConfigForKeys([
+  'CTF_BLOG_POST_TYPE_ID',
+  'CTF_SPACE_ID',
+  'CTF_CDA_ACCESS_TOKEN',
+  'CTF_CMA_ACCESS_TOKEN',
+  'CTF_PERSON_ID'
+])
+const {createClient} = require('./plugins/contentful')
+const cdaClient = createClient(ctfConfig)
+const cmaContentful = require('contentful-management')
+const cmaClient = cmaContentful.createClient({
+  accessToken: ctfConfig.CTF_CMA_ACCESS_TOKEN
+})
+
+const config = {
   /*
   ** Headers of the page
   */
@@ -49,9 +64,54 @@ module.exports = {
     },
     vendor: ['vue-scrollto']
   },
+
   plugins: [
+    { src: '~plugins/contentful' },
     { src: '~plugins/vue-scrollto' },
     { src: '~plugins/windowState', ssr: false },
     { src: '~plugins/ga.js', ssr: false }
-  ]
+  ],
+
+  /*
+  ** Get all blog posts from Contentful
+  ** and generate the needed files upfront
+  **
+  ** Included:
+  ** - blog posts
+  ** - available blog post tags
+  */
+  generate: {
+    routes () {
+      return Promise.all([
+        // get all blog posts
+        cdaClient.getEntries({
+          'content_type': ctfConfig.CTF_BLOG_POST_TYPE_ID
+        }),
+        // get the blog post content type
+        cmaClient.getSpace(ctfConfig.CTF_SPACE_ID)
+          .then(space => space.getContentType(ctfConfig.CTF_BLOG_POST_TYPE_ID))
+      ])
+      .then(([entries, postType]) => {
+        return [
+          // map entries to URLs
+          ...entries.items.map(entry => `/blog/${entry.fields.slug}`),
+          // map all possible tags to URLs
+          ...postType.fields.find(field => field.id === 'tags').items.validations[0].in.map(tag => `/tags/${tag}`)
+        ]
+      })
+    }
+  },
+
+    /*
+  ** Define environment variables being available
+  ** in generate and browser context
+  */
+  env: {
+    CTF_SPACE_ID: ctfConfig.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: ctfConfig.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: ctfConfig.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: ctfConfig.CTF_BLOG_POST_TYPE_ID
+  }
 }
+
+module.exports = config
