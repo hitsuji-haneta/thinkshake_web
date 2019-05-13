0feb58f23ad9efc250a0887a9062a746090ef443
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-05-13 23:00:23 +0900

    Update skill section

--- pages/top/Skills.vue
+++ pages/top/Skills.vue
@@ -1,92 +1,102 @@
 <template lang="pug">
   section#skills
     h1 skills
-    ul(class="circle_box")
-      li(class="circle_item") React.js
-      li(class="circle_item") React Native
-      li(class="circle_item") Redux
-      li(class="circle_item") Vue.js
-      li(class="circle_item") Nuxt.js
-      li(class="circle_item") Ruby on Rails
-      li(class="circle_item") Express
-      li(class="circle_item") Node.js
-      li(class="circle_item") Ethereum
-      li(class="circle_item") Solidity
-      li(class="circle_item") Python
-      li(class="circle_item") Go
-      li(class="circle_item") Python
-      li(class="circle_item") R
-      li(class="circle_item") Java
-      li(class="circle_item") GraphQL
-      li(class="circle_item") AWS
-      li(class="circle_item") GCP
-      li(class="circle_item") Firebase
-      li(class="circle_item") MySQL
-      li(class="circle_item") PostgreSQL
-      li(class="circle_item") MongoDB
+    .skills_wrapper
+      ul(class="circle_box" v-circle)
+        li(class="circle_item") JavaScript
+        li(class="circle_item") TypeScript
+        li(class="circle_item") Python
+        li(class="circle_item") Go
+        li(class="circle_item") GraphQL
+        li(class="circle_item") React.js
+        li(class="circle_item") React Native
+        li(class="circle_item") Redux
+        li(class="circle_item") Vue.js
+        li(class="circle_item") Nuxt.js
+        li(class="circle_item") Ruby on Rails
+        li(class="circle_item") Express
+        li(class="circle_item") Node.js
+        li(class="circle_item") EOS
+        li(class="circle_item") Ethereum
+        li(class="circle_item") Solidity
+        li(class="circle_item") AWS
+        li(class="circle_item") GCP
+        li(class="circle_item") Firebase
+        li(class="circle_item") MySQL
+        li(class="circle_item") PostgreSQL
+        li(class="circle_item") MongoDB
+        li(class="circle_item") Java
+        li(class="circle_item") CircleCI
+        li(class="circle_item") R
 </template>
 
 <script>
 import MyButton from '~/components/MyButton.vue'
 
 export default {
-  components: {
-    MyButton,
-  },
-  data() {
-    return {
-      isActive: false
+  directives: {
+    circle: {
+      inserted: function(el) {
+        const items = el.children
+        console.log(items.length);
+        const deg = 360 / items.length
+        const radian = deg * Math.PI / 180
+        const radius = 300
+        const center = 400
+        Array.forEach(items, (el, i) => {
+          const x = Math.cos(radian * i) * radius + 350
+          const y = Math.sin(radian * i) * radius + center
+          el.setAttribute("style", `top: ${y}px; left: ${x}px;`)
+        })
+      }
     }
   },
-  methods: {
-    onClick: function() {
-      location.href = 'mailto:s.takahashi@thinkshake.net'
-    }
-  }
 }
 </script>
 
 <style lang="stylus" scoped>
-  #skills
-    width 100%
-    display flex
-    justify-content top
-    text-align center
-    flex-direction column
-    align-items center
-    margin-bottom 30vh
+#skills
+  width 100%
+  height 100vh
+  display flex
+  justify-content top
+  text-align center
+  flex-direction column
+  align-items center
+  margin-bottom 30vh
+  overflow hidden
 
-  .circle_box
-    // position relative
-    margin 20px auto
-    display flex
-    justify-content start
-    flex-direction row
-    align-items center
-    align-content stretch
-    flex-wrap wrap
-    width 100%
-    // @media screen and (max-width: 500px)
-    //   width 90%
-    // @media screen and (min-width: 500px)
-    //   width 80%
+.skills_wrapper
+  position relative
+  overflow hidden
+  @media screen and (max-width: 500px)
+    right -50vw
+  @media screen and (min-width: 500px)
+    right 0
 
-  .circle_item
-    font-size 1.2rem
-    display inline-block
-    // width 100px
-    // border solid 1px
-    // position absolute
-    // width 85%
-    // margin-top 40px
-    // margin-bottom 40px
-    margin 20px 0
-    @media screen and (max-width: 500px)
-      width 100px
-    @media screen and (min-width: 500px)
-      width 200px
+.circle_box
+  position relative
+  width 800px
+  height 800px
+  animation drift 20000ms infinite linear
 
-  .form
-    height 300px
-    width 80%
+.circle_item
+  font-size 1.2rem
+  display inline-block
+  position absolute
+  animation drift-reverse 20000ms infinite linear
+  -webkit-transition all .5s
+  &:hover
+    color primary-color
+
+.form
+  height 300px
+  width 80%
+
+@keyframes drift
+  from { transform: rotate(0deg); }
+  from { transform: rotate(360deg); }
+@keyframes drift-reverse
+  from { transform: rotate(0deg); }
+  from { transform: rotate(-360deg); }
 </style>
